35584d78f16f88fee8416492d5e4fb1fd8e2ac10
duycuong96 <vuduycuong996@gmail.com>
2020-03-07 14:51:55 +0700

    update

--- database/migrations/2020_03_07_074036_create_users_table.php
+++ database/migrations/2020_03_07_074036_create_users_table.php
@@ -14,12 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
+            $table->date('dob');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar');
+            $table->integer('role')->default(config('common.role.user'));
             $table->timestamps();
         });
     }
